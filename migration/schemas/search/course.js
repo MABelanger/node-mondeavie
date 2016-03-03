372337ebd77460e013491b38a2f994647ecb35ca
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-03-03 18:29:10 -0500

    courses schemas

---
+++ migration/schemas/search/course.js
@@ -0,0 +1,100 @@
+// import the necessary modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var testingDaysSchema = new Schema({
+  "day": String,
+  "isFull": Boolean
+});
+
+var SchedulesSchema = new Schema({
+  "isFull": Boolean,
+  "hourStart": String,
+  "hourEnd": String,
+  "dayEnd": String,
+  "dayStart": String,
+  "dayName": String,
+  "testingDays": [ testingDaysSchema ]
+});
+
+var courseTypesSchema = new Schema({
+  "name": String,
+  "description": String,
+  "schedules": [ SchedulesSchema ]
+});
+
+var CourseSchema = new Schema({
+  "courseType": String,
+  "note": String,
+  "image": String,
+  "description": String,
+  "price": String,
+  "isVisible": Boolean,
+  "courseTypes": [ courseTypesSchema ]
+});
+
+var TeachersSchema = new Schema({
+  "firstName": String,
+  "lastName": String,
+  "tel": String,
+  "schoolName": String,
+  "schoolUrl": String,
+  "course": CourseSchema
+});
+
+
+// create an export function to encapsulate the model creation
+module.exports = function() {
+  var coursesSchema = Schema({
+    name : String,
+    svg: String,
+    teachers : [ TeachersSchema ]
+  }); // courseSchema
+  mongoose.model('Courses', coursesSchema);
+};
+
+
+
+/*
+  {
+    "name": "Yoga",
+    "svg" : "",
+    "teachers": [
+      {
+        "firstName": "Isabelle",
+        "lastName": "Nadeau",
+        "tel": "514-919-6318",
+        "schoolName": "",
+        "schoolUrl": "",
+        "course": {
+          "courseType": "Yoga doux",
+          "note": "",
+          "image": "/media/course_pic/Isabelle_Nadeau_300x300.jpg",
+          "description": "",
+          "price": "",
+          "isVisible": true,
+          "courseTypes": [
+            {
+              "name": "Yoga sur chaise",
+              "description": "",
+              "schedules": [
+                {
+                  "isFull": false,
+                  "hourStart": "13:00:00",
+                  "hourEnd": "14:15:00",
+                  "dayEnd": "2016-06-13",
+                  "dayStart": "2016-01-12",
+                  "dayName": "mardi",
+                  "testingDays": [
+                    {
+                      "day": "2016-01-26",
+                      "isFull": false
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      },
+*/
