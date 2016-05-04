067cbfb8497ce490e8c4cf3a61770019e2681363
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-05-03 21:32:35 -0400

    end convert

--- data/migration/conference/convertCourses.js
+++ data/migration/conference/convertCourses.js
@@ -1,7 +1,80 @@
 "use strict";
 var _ = require('lodash');
-
+var moment = require('moment');
+var Request     = require("superagent");
 var json = require('./course10.json');
+
+          
+/*
+ * Free Days
+ */
+
+
+function getTestingDaysList(testingDays){
+  // let testingDays = {
+  //   list: {
+  //     5: {
+  //       list: [
+  //         "05 mai 2016",
+  //         "06 mai 2016"
+  //       ],
+  //       monthName: "Mai 2016"
+  //     }
+  //   }
+  // }
+
+  // {"list":{"5":{"list":["05 mai 2016","06 mai 2016"],"monthName":"Mai 2016"}}}
+  json = JSON.stringify(testingDays);
+
+  // get only string between []
+  let listStr = json.substring(json.lastIndexOf("[")+1,json.lastIndexOf("]"));
+  // put the []
+  listStr = "[" + listStr + "]"
+
+  // return the array
+  return JSON.parse(listStr);
+}
+
+
+
+
+
+
+function getFreeDay(json){
+  let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+
+  let strDate = json + " " + "00:00";
+
+  //let mDate = moment('5 mai 2016 09:30', 'DD MMM YYYY HH:mm', 'fr');
+  let mDate = moment(strDate, 'DD MMM YYYY HH:mm', 'fr');
+
+
+  let noOffset = moment(mDate - tzoffset).utcOffset(0);
+
+  return {
+    day : noOffset.toISOString(),
+    isFull: false
+  }
+}
+
+
+function addFreeDay(freeDays, json){
+
+  // remove the junk, get only the list between []
+  json = getTestingDaysList(json);
+
+  //console.log('____json', json)
+  for(let i=0; i<json.length; i++){
+    let freeDay = _.find(freeDays, function(freeDay) {
+      return freeDay.day == getFreeDay(json[i]).day; 
+    });
+
+    if(!freeDay){
+      freeDay = getFreeDay(json[i]);
+      freeDays.push(freeDay);
+    }
+  }
+}
 
 
 /*
@@ -32,18 +105,20 @@ function getSchedule(json){
 function addSchedule(schedules, json){
 
 
+  //console.log('____json', json)
   for(let i=0; i<json.length; i++){
-    //console.log('json', json[0])
     let schedule = _.find(schedules, function(schedule) {
       return schedule.dayStart == getSchedule(json[i]).dayStart; 
     });
 
     if(!schedule){
       schedule = getSchedule(json[i]);
+      schedule.freeDays = [];
       schedules.push(schedule);
     }
-  }
 
+    addFreeDay(schedule.freeDays, json[i].testingDays);
+  }
 }
 
 
@@ -67,15 +142,18 @@ function addCourseType(courseTypes, json){
 
 
   for(let i=0; i<json.length; i++){
-    //console.log('json', json[0])
     let courseType = _.find(courseTypes, function(courseType) {
       return courseType.name == getCourseType(json[i]).name; 
     });
 
     if(!courseType){
       courseType = getCourseType(json[i]);
+      courseType.schedules = [];
       courseTypes.push(courseType);
     }
+
+    
+    addSchedule(courseType.schedules, json[i].daySchedules)
   }
 
 }
@@ -162,7 +240,7 @@ function getCourse(json){
   return course;
 }
 
-let courses = [];
+var courses = [];
 function addCourse(json){
   let course = _.find(courses, function(course) {
     return course.name == getCourse(json.course_name).name; 
@@ -176,70 +254,38 @@ function addCourse(json){
 
   addTeacher(course.teachers, json);
 
-  console.log('course',  JSON.stringify(course))
+  //console.log('course',  JSON.stringify(course))
+
 }
 
-addCourse(json);
+function readUrl(url, cb) {
+  Request
+  .get(url, function(err, res){
+    if(!err){
+      cb(res.body);
+    }
+  });
+}
 
+for(let i=0; i<10; i++){
+  let url = 'http://www.mondeavie.ca/calendar_activities/api/nested/childs/courses/'+ i + '?format=json';
+  readUrl(url, function(json){
+    addCourse(json);
 
-/*{
-    name: null,
-    svg: null,
-    slug: null,
-    teachers: [
-      {
-        slug: null,
-        firstName: null,
-        lastName: null,
-        tel: null,
-        schoolName: null,
-        schoolUrl: null,
-        "course": {
-          slug: null,
-          courseType: null,
-          note: null,
-          price: null,
-          isVisible: true,
-          image: {
-            url: "media/img/course_description/marie-andree-beaumier_medicinale.jpg"
-          }
-          courseTypes: [
-            {
-              name: null,
-              description: null,
-              _id: null,
-              slug: null,
-              schedules: [
-                {
-                  isFull: false,
-                  dayEnd: null,
-                  dayStart: null,
-                  dayName: null,
-                  freeDays: [
-                    {
-                      day: null,
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-      }
-    ]
+    // convertConference(json, function(conference){
+    //   console.log('conference._id', conference._id);
+    // });
+    if(i==9){
+      endConvert()
+    }
+  });
+}
+
+function endConvert(){
+  for(let i=0; i<courses.length; i++){
+    console.log('courses[i]:', courses[i])
   }
 }
-*/
 
-
-
-// for(let i=0; i<100; i++){
-//   let url = 'http://www.mondeavie.ca/calendar_activities/api/nested/childs/conferences/'+ i + '?format=json';
-//   readDayConference(url, function(json){
-//     convertConference(json, function(conference){
-//       console.log('conference._id', conference._id);
-//     });
-//   });
-// }
 
 
