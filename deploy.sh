e2101592a00ef64af9d4461bde0989e06c46492b
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2016-05-15 23:04:47 -0400

    deploy on amazon

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,20 @@
 
 set -o errexit # Exit on error
 
+cd ../react-admin
+echo "pull react-admin"
+git pull
+echo "rebuild react-admin"
+npm run deploy
+
+cd ../react-calendar
+echo "pull react-calendar"
+git pull
+echo "rebuild react-calendar"
+npm run deploy
+
+cd ../node-mondeavie
+echo "copy react-admin react-calendar into app/dist"
 rm -rf ./app/
 mkdir -p ./app/dist/
 cp -a ../react-admin/dist ./app/dist/react-admin
